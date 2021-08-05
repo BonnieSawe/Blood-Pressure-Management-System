4d206abf2ce822aec54c192e7e4be6d911129ed4
Boniface Sawe <kiplangatbonifaces@gmail.com>
2021-08-05 18:38:26 +0300

    fixing tests

--- tests/Feature/PatientTest.php
+++ tests/Feature/PatientTest.php
@@ -3,8 +3,13 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\Role;
 use App\Models\User;
+use Livewire\Livewire;
+use Faker\Factory as Faker;
 use Illuminate\Support\Carbon;
+use App\Http\Livewire\CreatePatient;
+use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,43 +19,44 @@ class PatientTest extends TestCase
 
     public function test_only_patient_screen_can_be_rendered_to_a_logged_in_user()
     {
+        $role = Role::create(['name' => 'Admin']);
+
         $response = $this->get('/patients');
 
         $response->assertStatus(302);
 
         $user = User::factory()->create();
 
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
+        Auth::loginUsingId($user->id);
+
+        $response = $this->get('/patients');
 
         $response->assertStatus(200);
     }
 
     public function test_users_can_be_added_by_logged_in_user()
     {
-        $response = $this->get('/patients');
+        $faker = Faker::create();
 
-        $response->assertStatus(302);
+        $role = Role::create(['name' => 'Admin']);
 
         $user = User::factory()->create();
 
-        $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
+        Auth::loginUsingId($user->id);
 
         $gender_options = ['male', 'female'];
+
         $gender = array_rand((array) $gender_options, (int) 1);
 
-        $response = $this->post('/patients/create', [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'phone' => $this->faker->e164PhoneNumber,
-            'gender' => $gender_options[$gender],
-            'dob' => Carbon::parse($this->faker->dateTimeBetween('1960-01-01', '2020-12-31'))
-        ]);
+        $response = Livewire::test(CreatePatient::class)
+            ->set([
+                'form.name' => $faker->name(),
+                'form.email' => $faker->unique()->safeEmail(),
+                'form.phone' => $faker->e164PhoneNumber,
+                'form.gender' => $gender_options[$gender],
+                'form.dob' => Carbon::parse($faker->dateTimeBetween('1960-01-01', '2020-12-31'))
+            ])
+            ->call('submit');
 
         $response->assertStatus(200);
     }
